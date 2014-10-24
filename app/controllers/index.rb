a5d77daca3bda27d822fbc4b909d2bc6d7670acb
Code Division <codedivision@11th.ventures>
2014-10-24 14:32:04 +0800

    Category links included

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,13 @@
 get '/' do
   # Look in app/views/index.erb
-    @category = Category.all.map{|item| item.name}
+  @category = Category.all
   erb :index
+end
+
+
+get %r{/([\d]+)$} do
+  category = Category.find(request.path[1..-1].to_i)
+  @category_name = category.name
+  @posts = category.posts
+  erb :category
 end
